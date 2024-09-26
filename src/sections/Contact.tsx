b21045ddc74dad2f7b7768297b2053f821bcc6b7
mahendragalih26 <mahendragalih26@gmail.com>
2024-09-26 07:44:50 +0700

    add : slicing contact UI

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -11,7 +11,7 @@ export const ContactSection = () => {
               backgroundImage: `url${grainImage.src}`,
             }}
           ></div>
-          <div className="flex flex-col md:flex-row gap-8 md:gap-16 items-center ">
+          <div className="flex flex-col md:flex-row gap-8 md:gap-10 items-center ">
             <div>
               <h2 className="font-serif text-2xl md:text-3xl">
                 Let's create something amazing together
@@ -21,9 +21,11 @@ export const ContactSection = () => {
                 discus! how I can help you achieve your goals.
               </p>
             </div>
-            <button className="text-white bg-gray-900 inline-flex items-center px-6 h-12 rounded-xl gap-2 w-max border border-gray-900">
-              <span className="font-semibold">Contact Me</span>
-              <ArrowUpRightIcon className="size-4" />
+            <button className="text-white bg-gray-900 px-6 h-12 rounded-xl gap-2 w-max min-w-44 border border-gray-900">
+              <div className="inline-flex  items-center gap-2">
+                <span className="font-semibold">Contact Me</span>
+                <ArrowUpRightIcon className="size-4" />
+              </div>
             </button>
           </div>
         </div>
