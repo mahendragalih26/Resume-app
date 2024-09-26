f39a5de342c3930216f2f5c812cad4c434196f62
mahendragalih26 <mahendragalih26@gmail.com>
2024-09-26 12:16:11 +0700

    add : walkthrough animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+import Preloader from "@/components/Preloader"
 import { AboutSection } from "@/sections/About"
 import { ContactSection } from "@/sections/Contact"
 import { Footer } from "@/sections/Footer"
@@ -6,10 +8,27 @@ import { HeroSection } from "@/sections/Hero"
 import { ProjectsSection } from "@/sections/Projects"
 import { TapeSection } from "@/sections/Tape"
 import { TestimonialsSection } from "@/sections/Testimonials"
+import { AnimatePresence } from "framer-motion"
+import { useEffect, useState } from "react"
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    ;(async () => {
+      setTimeout(() => {
+        setIsLoading(false)
+        document.body.style.cursor = "default"
+        window.scrollTo(0, 0)
+      }, 2000)
+    })()
+  }, [])
+
   return (
     <div>
+      <AnimatePresence mode="wait">
+        {isLoading && <Preloader />}
+      </AnimatePresence>
       <Header />
       <HeroSection />
       <ProjectsSection />
